e1308c2d2d2d0e820488c3355e48c4594a1ede8b
Justin Meyer <justinbmeyer@gmail.com>
2016-11-01 12:27:43 -0500

    some basics of the old theme working

---
+++ make-example.js
@@ -0,0 +1,27 @@
+var generate = require("bit-docs-generate-html/generate");
+var Q = require("q");
+var fs = require("fs");
+var readFile = Q.denodeify(fs.readFile);
+var path = require("path");
+
+var docMap = readFile(__dirname+"/docMap.json").then(function(source){
+    return JSON.parse(""+source);
+});
+
+var forceBuild = process.argv.indexOf("-f") !== -1;
+
+generate(docMap,{
+    html: {
+        templates: path.join(__dirname, "templates"),
+        dependencies: {
+            "bit-docs-docjs-theme": __dirname,
+            "bit-docs-prettify": "^0.1.0",
+			"bit-docs-html-highlight-line": "^0.2.2"
+        }
+    },
+    dest: path.join(__dirname, "temp"),
+    parent: "StealJS",
+    forceBuild: forceBuild,
+	minifyBuild: false,
+    debug: true
+}).done();
---
+++ readme.md
@@ -0,0 +1,3 @@
+```
+node ./make-example.js -f
+```
